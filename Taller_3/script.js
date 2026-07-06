8b0c413b57e7ed0160309fc2d22285c77d27febb
chiquitojosh4 <chiquitojosh4@gmail.com>
2026-07-05 21:14:59 -0500

    Add files via upload

---
+++ Taller_3/script.js
@@ -0,0 +1,17 @@
+/*var maxValue = Number.MAX_VALUE;
+var minValue = Number.MIN_VALUE;
+
+alert("max value: "+ maxValue);
+alert("min value: "+ minValue);
+alert1("valor especial: "+ maxValue*2);*/
+
+/*var operando_1 = eval(prompt("introduce el primer valor logico(true o false):", true));
+var operando_2 = eval(prompt("introduce el segundo valor logico(true o false):", false));
+var resultado_logico = operando_1 && operando_2;
+alert("resultado: " + resultado_logico);*/
+
+var countdown = prompt("indique un numero para indicar la cuenta :");
+while (countdown>0){
+    alert(countdown+"...");
+    countdown--;
+}
